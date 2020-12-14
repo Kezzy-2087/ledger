8819b5b3f65ed9ce94b7217ebc21f4132826713d
Oluwanifemi Alowolodu <70798076+Kezzy-2087@users.noreply.github.com>
2020-12-14 15:02:47 +0100

    Update saver.php

--- saver.php
+++ saver.php
@@ -6,16 +6,20 @@ $dateled= 	strtoupper($dateled);
 
 $conn = mysqli_connect("localhost","root","","ledgerdb") or die(mysql_error());
 
-$str = "SELECT dateled FROM ledgerinfo WHERE dateled ='$dateled'";
+$str = "SELECT dateled FROM journal WHERE dateled ='$dateled'";
 $query = mysqli_query($conn,$str);
+if(!$query){
+	die ("sql error");
+}
 if (mysqli_num_rows($query)==1){
     echo "Entry Successful - Note: date exists";
 }else{
     # Insert record as a new record
-   $str = "INSERT INTO ledgerinfo(dateled,timeled,itemdescr,particulars,quantity,price,trantype,cusname) VALUES 
-            ('$dateled','$timeled','$itemdescr','$particulars','$quantity','$price','$trantype','$cusname')";
-             echo "Entry Successful";
+   $str = "INSERT INTO journal(dateled,timeled,itemdescr,particulars,quantitee,price,trantype,cusname) VALUES 
+        ('$dateled','$timeled','$itemdescr','$particulars','$quantitee','$price','$trantype','$cusname')";
+        echo "Entry Successful";
 	}
+
     
-        $query = mysqli_query($conn,$str);
+    $query = mysqli_multi_query($conn,$str);
 ?>
